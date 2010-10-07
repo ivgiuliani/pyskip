33106db23a1b29a9b9b2ab87be4c2e52da21c682
Ivan Giuliani <giuliani.v@gmail.com>
2010-10-07 12:28:53 +0200

    Better setup file

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
 from distutils.core import setup, Extension
 
-module = Extension('skip',
-                   sources = ['skip.c'],
-         )
+module = Extension(
+    "skip",
+    sources = ["skip.c"],
+)
 
-setup (name = 'pyskip',
-       version = '0.1',
-       author = 'Ivan Giuliani',
-       author_email = 'giuliani.v@gmail.com',
-       description = 'A skip list implementation for Python',
-       ext_modules = [module])
+setup(  name = "pyskip",
+        version = "0.1",
+        author = "Ivan Giuliani",
+        author_email = "giuliani.v@gmail.com",
+        description = "A skip list implementation for Python",
+        url="http://github.com/kratorius/pyskip",
+        license="GPLv3",
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Environment :: Console",
+            "Intended Audience :: Developers",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: GNU General Public License (GPL)",
+            "Operating System :: POSIX",
+            "Operating System :: POSIX :: Linux",
+            "Operating System :: Microsoft",
+            "Operating System :: MacOS",
+            "Programming Language :: C",
+            "Topic :: Software Development :: Libraries",
+        ],
+        ext_modules = [module]
+)
