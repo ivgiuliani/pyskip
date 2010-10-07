8af99260409c9104331b020f8872b799e2a0aca9
Ivan Giuliani <giuliani.v@gmail.com>
2010-10-07 20:48:20 +0200

    Check that SkipDict behaves like a dict

--- tests/correctness.py
+++ tests/correctness.py
@@ -28,3 +28,21 @@ class BasicTest(unittest.TestCase):
         self.assertEqual(self.skip["key"], "modified")
 
 
+class BehaveLikeDictTest(unittest.TestCase):
+    "Check that the SkipDict has the same behavior of a dictionary"
+
+    def setUp(self):
+        self.skip = SkipDict()
+
+    def test_len(self):
+        "Test len() on SkipDict"
+        self.skip[0] = 0
+        self.skip[1] = 1
+        self.skip[2] = 2
+        self.skip[3] = 3
+        self.assertEqual(len(self.skip), 4)
+
+    def test_assignment(self):
+        "Check __getitem__/__setitem__ assignment (through dict['key'])"
+        self.skip["key"] = "value"
+        self.assertEqual(self.skip["key"], "value")
